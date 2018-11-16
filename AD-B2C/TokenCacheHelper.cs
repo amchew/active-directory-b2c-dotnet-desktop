649c9c7698a0e23b36367012211feb54343036f2
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-11-16 17:38:48 +0300

    Remove extra code

--- AD-B2C/TokenCacheHelper.cs
+++ AD-B2C/TokenCacheHelper.cs
@@ -25,8 +25,6 @@
 //
 //------------------------------------------------------------------------------
 
-using System.IO;
-using System.Security.Cryptography;
 using Microsoft.Identity.Client;
 
 namespace AD_B2C
@@ -45,48 +43,8 @@ namespace AD_B2C
             if (usertokenCache == null)
             {
                 usertokenCache = new TokenCache();
-              //  usertokenCache.SetBeforeAccess(BeforeAccessNotification);
-              //  usertokenCache.SetAfterAccess(AfterAccessNotification);
             }
             return usertokenCache;
-        }
-
-        /// <summary>
-        /// Path to the token cache
-        /// </summary>
-        public static readonly string CacheFilePath = ".msalcache.bin";
-
-        private static readonly object FileLock = new object();
-
-        public static void BeforeAccessNotification(TokenCacheNotificationArgs args)
-        {
-           /* lock (FileLock)
-            {
-                args.TokenCache.Deserialize(File.Exists(CacheFilePath)
-                    ? ProtectedData.Unprotect(File.ReadAllBytes(CacheFilePath),
-                                              null,
-                                              DataProtectionScope.CurrentUser)
-                    : null);
-            }*/
-        }
-
-        public static void AfterAccessNotification(TokenCacheNotificationArgs args)
-        {
-            // if the access operation resulted in a cache update
-           /* if (args.TokenCache.HasStateChanged)
-            {
-                lock (FileLock)
-                {
-                    // reflect changesgs in the persistent store
-                    File.WriteAllBytes(CacheFilePath,
-                                       ProtectedData.Protect(args.TokenCache.Serialize(),
-                                                             null,
-                                                             DataProtectionScope.CurrentUser)
-                                      );
-                    // once the write operationtakes place restore the HasStateChanged bit to filse
-                    args.TokenCache.HasStateChanged = false;
-                }
-            }*/
-        }
+        }       
     }
 }
